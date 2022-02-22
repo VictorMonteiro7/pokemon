01b350c2fdd21fe194c87dfd42eed7e7e9e5d659
Victor Monteiro <contato@ovictormonteiro.com>
2022-02-21 21:24:57 -0300

    Pokecard feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import {MainRoutes} from './routes/MainRoutes';
+
 function App() {
   return (
-    <></>
+    <>
+    <h1>Seja bem vindo ao Universo dos Pokemons</h1>
+    <MainRoutes />
+    <footer>
+      <p>&copy;Victor Monteiro - Alguns direitos reservados.</p>
+    </footer>
+    </>
   );
 }
 
